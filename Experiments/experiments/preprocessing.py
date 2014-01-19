e45d1b5c23ae69e9940a0ee0d18d92cd067756e0
Andre Lynum <and-ly@online.no>
2014-01-19 22:53:16 +0100

    Mahoney style preprocessing.

--- Experiments/experiments/preprocessing.py
+++ Experiments/experiments/preprocessing.py
@@ -1,2 +1,22 @@
+import re
+
+
 def mahoney_clean(str):
-    return ""
+    str = " " + str + " "
+    str = str.lower()
+
+    str = re.sub('1', ' one ', str, flags=re.MULTILINE)
+    str = re.sub('2', ' two ', str, flags=re.MULTILINE)
+    str = re.sub('3', ' three ', str, flags=re.MULTILINE)
+    str = re.sub('4', ' four ', str, flags=re.MULTILINE)
+    str = re.sub('5', ' five ', str, flags=re.MULTILINE)
+    str = re.sub('6', ' six ', str, flags=re.MULTILINE)
+    str = re.sub('7', ' seven ', str, flags=re.MULTILINE)
+    str = re.sub('8', ' eight ', str, flags=re.MULTILINE)
+    str = re.sub('9', ' nine ', str, flags=re.MULTILINE)
+    str = re.sub('0', ' zero ', str, flags=re.MULTILINE)
+
+    str = re.sub('[^a-z]', ' ', str, flags=re.MULTILINE)
+    str = re.sub('\s+', ' ', str, flags=re.MULTILINE)
+
+    return str
--- Experiments/experiments/test/test_preprocessing.py
+++ Experiments/experiments/test/test_preprocessing.py
@@ -3,19 +3,18 @@ import unittest
 from experiments.preprocessing import mahoney_clean
 
 
-enwiki_input = "'''Anarchism''' originated as a term of abuse first used against early [[working class]] [[radical]]s" + \
-               " including the [[Diggers]] of the [[English Revolution]] and the [[sans-culotte|''sans-culottes'']] " + \
-               "of the [[French Revolution]].[http://uk.encarta.msn.com/encyclopedia_761568770/Anarchism.html] Whilst" + \
-               " the term is still used in a pejorative way to describe ''&quot;any act that used violent means to " + \
-               "destroy the organization of society&quot;''&lt;ref&gt;[http://www.cas.sc.edu/socy/faculty/deflem/" + \
-               "zhistorintpolency.html History of International Police Cooperation], from the final protocols of the" + \
-               " &quot;International Conference of Rome for the Social Defense Against Anarchists&quot;, " + \
-               "1898&lt;/ref&gt;, it has also been taken up as a positive label by self-defined anarchists."
+enwiki_input = "'''Anarchism''' originated as a term of abuse first used against early [[working class]] radicals" + \
+               " including the [[Diggers]] of the [[English Revolution]] and the sans-culottes " + \
+               "of the [[French Revolution]]. Whilst" + \
+               " the term is still used in a pejorative way to describe ''any act that used violent means to " + \
+               "destroy the organization of society, it has also been taken up as a positive label by " + \
+               "self-defined anarchists. In 1998."
 
 fil_output = " anarchism originated as a term of abuse first used against early working class radicals including the " + \
              "diggers of the english revolution and the sans culottes of the french revolution whilst the term is " + \
              "still used in a pejorative way to describe any act that used violent means to destroy the organization " + \
-             "of society it has also been taken up as a positive label by self defined anarchists"
+             "of society it has also been taken up as a positive label by self defined anarchists in " + \
+             "one nine nine eight "
 
 
 class PreprocessingTestCase(unittest.TestCase):
