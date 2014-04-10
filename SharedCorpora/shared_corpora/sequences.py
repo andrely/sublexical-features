a4e58e48c69829a0cbf3e7a492f592258b36bc5a
André Lynum <and-ly@online.no>
2014-04-10 23:44:02 +0200

    New project structure.

--- SharedCorpora/shared_corpora/sequences.py
+++ SharedCorpora/shared_corpora/sequences.py
@@ -1,4 +1,34 @@
+from abc import abstractmethod, ABCMeta
 from collections import Sequence, Iterable
+
+
+class SlicableSequence(Sequence):
+    __metaclass__ = ABCMeta
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            return [self._get_index(i) for i in xrange(*index.indices(len(self)))]
+        elif isinstance(index, Iterable):
+            return [self._get_index(i) for i in index]
+        elif isinstance(index, int):
+            return self._get_index(index)
+        else:
+            raise TypeError
+
+    @abstractmethod
+    def _get_index(self, index):
+        raise NotImplementedError
+
+
+class ListSequence(SlicableSequence):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def _get_index(self, index):
+        return self.data[index]
 
 
 class FilteredSequence(Sequence):
---
+++ SublexicalSemantics/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+# noinspection PyUnresolvedReferences
+import multiprocessing
+
+setup(
+    name='SublexicalSemantics',
+    version='0.1',
+    packages=['sublexical_semantics'],
+    url='',
+    license='',
+    author='Andre Lynum',
+    author_email='',
+    description=''
+)
