e4538bae333b5d7b5df7f87d6fd30e36578c4c2a
Andre Lynum <and-ly@online.no>
2014-05-12 21:40:51 +0200

    Reuters RCV1 baseline experiment.

---
+++ Experiments/experiments/reuters_rcv1.py
@@ -0,0 +1,83 @@
+import logging
+from optparse import OptionParser
+import sys
+
+from numpy import mean, std
+from sklearn.cross_validation import train_test_split, cross_val_score, KFold
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import f1_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelBinarizer
+
+from shared_corpora.reuters_rcv1 import newsitem_gen, TOP5_TOPICS
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    parser = OptionParser()
+    parser.add_option("-c", "--corpus-path")
+    parser.add_option("-p", "--processors", type="int", default=1)
+
+    opts, args = parser.parse_args()
+
+    if not opts.corpus_path:
+        print parser.print_usage()
+        sys.exit(1)
+    else:
+        corpus_path = opts.corpus_path
+
+    num_proc = opts.processors
+
+    logging.info("Using corpus path %s" % corpus_path)
+    logging.info("Using %d processors" % num_proc)
+
+    texts = []
+    target = []
+
+    topics = set(TOP5_TOPICS)
+
+    doc_count = 0
+
+    logging.info("Collecting articles with top5 topics")
+
+    for newsitem in newsitem_gen(corpus_path):
+        if newsitem.has_key('topics') and not topics.isdisjoint(newsitem['topics']):
+            doc_count += 1
+
+            texts.append('\n'.join(newsitem['text']))
+            target.append(list(topics.intersection(newsitem['topics'])))
+
+            if doc_count % 10000 == 0:
+                logging.info("Added %d documents..." % doc_count)
+
+    logging.info("Using %d documents." % doc_count)
+
+    logging.info("Binarizing topics")
+    binarizer = LabelBinarizer()
+    y = binarizer.fit_transform(target)
+
+    logging.info("Creating train/test split")
+    texts_train, texts_test, y_train, y_test = train_test_split(texts, y, test_size=0.2)
+
+    logging.info("Running 10-fold cross validation")
+    model = Pipeline([('vect', TfidfVectorizer(decode_error='ignore', strip_accents='unicode',
+                                               max_df=0.5, min_df=5, sublinear_tf=True)),
+                      ('nb', OneVsRestClassifier(MultinomialNB()))])
+    cv_scores = cross_val_score(model, texts_train, y_train, n_jobs=num_proc, cv=KFold(len(texts_train), n_folds=10),
+                                verbose=1, scoring='f1')
+
+    print "10-fold cv score: %.04f +/- %.04f" % (mean(cv_scores), std(cv_scores))
+
+    logging.info("Evaluating model")
+    model = Pipeline([('vect', TfidfVectorizer(decode_error='ignore', strip_accents='unicode',
+                                               max_df=0.5, min_df=5, sublinear_tf=True)),
+                      ('nb', OneVsRestClassifier(MultinomialNB()))])
+    model.fit(texts_train, y_train)
+
+    pred = model.predict(texts_test)
+    score = f1_score(y_test, pred)
+
+    print "Evaluation score: %.04f" % score
--- SharedCorpora/shared_corpora/reuters_rcv1.py
+++ SharedCorpora/shared_corpora/reuters_rcv1.py
@@ -1,12 +1,127 @@
+from collections import Counter
 from glob import glob
 import logging
 import os
-from zipfile import ZipFile, zlib
+from zipfile import ZipFile, zlib, BadZipfile
 
 from lxml import etree
 
 
+
+
+# Reuters RCV1
+# 803441 news items
+# Average of 3.21 topics assigned to each article
+# 103 different topics
+
+# Topic distribution:
+# CCAT 373856
+# GCAT 234659
+# MCAT 199850
+# C15 149958
+# ECAT 117433
+# M14 84953
+# C151 81796
+# C152 72952
+# GPOL 56840
+# M13 52883
+# C18 51423
+# M11 48625
+# M141 47625
+# C181 43320
+# E21 43106
+# C17 41780
+# C31 40446
+# GDIP 37707
+# C13 37362
+# GSPO 35315
+# GVIO 32586
+# GCRIM 32191
+# C24 32118
+# M131 28145
+# E212 27396
+# E12 27055
+# M132 26701
+# M12 25994
+# C21 25367
+# C11 24297
+# C1511 23200
+# M143 21914
+# E51 20689
+# G15 19136
+# C171 18287
+# GJOB 17216
+# E41 16876
+# E211 15755
+# C33 15310
+# E512 12615
+# M142 12115
+# C12 11935
+# C42 11857
+# GVOTE 11530
+# C172 11475
+# C41 11341
+# C411 10262
+# GDEF 8834
+# GDIS 8635
+# E11 8561
+# G154 8398
+# C183 7403
+# C14 7393
+# C312 6639
+# E13 6340
+# GENV 6252
+# C22 6114
+# GHEA 6026
+# C174 5869
+# E131 5655
+# GPRO 5494
+# E71 5264
+# C34 4827
+# C182 4667
+# C311 4297
+# G158 4293
+# GWEA 3868
+# GENT 3798
+# G151 3302
+# E511 2923
+# GREL 2846
+# GODD 2793
+# C173 2632
+# C23 2621
+# GSCI 2406
+# G153 2359
+# E31 2340
+# E513 2290
+# E121 2181
+# E411 2135
+# G155 2123
+# G152 2103
+# E14 2083
+# C32 2081
+# G157 2034
+# C16 1918
+# GWELF 1867
+# E311 1699
+# C331 1210
+# E143 1205
+# C313 1108
+# E132 936
+# GOBIT 844
+# GTOUR 677
+# E61 391
+# E141 376
+# GFAS 312
+# G156 260
+# E142 200
+# E313 111
+# E312 52
+# G159 40
+# GMIL 5
+
 SUBDIRS = ['100623_0655', '110104_0810']
+
+TOP5_TOPICS = ['CCAT', 'GCAT', 'MCAT', 'C15', 'ECAT']
 
 
 def unpack_collection(path):
@@ -21,7 +136,7 @@ def unpack_collection(path):
         with ZipFile(fn) as z:
             try:
                 z.extractall(dest_path)
-            except zlib.error:
+            except (zlib.error, BadZipfile):
                 logging.warn("Failed to inflate %s" % fn)
 
 
@@ -84,7 +199,8 @@ def read_newsitem(fn):
     if newsitem_elt.find('byline') != None:
         newsitem['byline'] = newsitem_elt.find('byline').text
     else:
-        logging.warning("no byline found in %s" % fn)
+        pass
+        # logging.warning("no byline found in %s" % fn)
 
     if newsitem_elt.find('text') != None:
         newsitem['text'] = [p_elt.text for p_elt in newsitem_elt.find('text').findall('p')]
@@ -102,3 +218,28 @@ def read_newsitem(fn):
 
     return newsitem
 
+
+def newsitem_gen(corpus_path):
+    for fn in corpus_fns(corpus_path):
+        yield read_newsitem(fn)
+
+
+def topic_statistics(corpus_path):
+    topic_counter = Counter()
+    topic_avg = 0.0
+    newsitem_count = 0
+
+    for newsitem in newsitem_gen(corpus_path):
+        try:
+            topics = newsitem['topics']
+            newsitem_count += 1
+
+            topic_counter.update(topics)
+            topic_avg += (len(topics) - topic_avg) / newsitem_count
+
+            if newsitem_count % 10000 == 0:
+                logging.info("Processed %d newsitems..." % newsitem_count)
+        except KeyError:
+            continue
+
+    return newsitem_count, topic_avg, topic_counter
