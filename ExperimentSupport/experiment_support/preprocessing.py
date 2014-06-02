9c6bde2f975a7afeb4cefbb78a94f520ccc989ed
André Lynum <and-ly@online.no>
2014-06-02 22:22:40 +0200

    Compiled regexes in mahoney_clean().

--- ExperimentSupport/experiment_support/preprocessing.py
+++ ExperimentSupport/experiment_support/preprocessing.py
@@ -2,24 +2,36 @@ import re
 
 from nltk import ngrams
 
+RE_ONE = re.compile('1', flags=re.MULTILINE)
+RE_TWO = re.compile('2', flags=re.MULTILINE)
+RE_THREE = re.compile('3', flags=re.MULTILINE)
+RE_FOUR = re.compile('4', flags=re.MULTILINE)
+RE_FIVE = re.compile('5', flags=re.MULTILINE)
+RE_SIX = re.compile('6', flags=re.MULTILINE)
+RE_SEVEN = re.compile('7', flags=re.MULTILINE)
+RE_EIGHT = re.compile('8', flags=re.MULTILINE)
+RE_NINE = re.compile('9', flags=re.MULTILINE)
+RE_ZERO = re.compile('0', flags=re.MULTILINE)
+RE_NOALPHA = re.compile('[^a-z]', flags=re.MULTILINE)
+RE_WSPACE = re.compile('\s+', flags=re.MULTILINE)
 
 def mahoney_clean(text_str):
     text_str = " " + text_str + " "
     text_str = text_str.lower()
 
-    text_str = re.sub('1', ' one ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('2', ' two ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('3', ' three ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('4', ' four ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('5', ' five ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('6', ' six ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('7', ' seven ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('8', ' eight ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('9', ' nine ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('0', ' zero ', text_str, flags=re.MULTILINE)
+    text_str = RE_ONE.sub(' one ', text_str)
+    text_str = RE_TWO.sub(' two ', text_str)
+    text_str = RE_THREE.sub(' three ', text_str)
+    text_str = RE_FOUR.sub(' four ', text_str)
+    text_str = RE_FIVE.sub(' five ', text_str)
+    text_str = RE_SIX.sub(' six ', text_str)
+    text_str = RE_SEVEN.sub(' seven ', text_str)
+    text_str = RE_EIGHT.sub(' eight ', text_str)
+    text_str = RE_NINE.sub(' nine ', text_str)
+    text_str = RE_ZERO.sub(' zero ', text_str)
 
-    text_str = re.sub('[^a-z]', ' ', text_str, flags=re.MULTILINE)
-    text_str = re.sub('\s+', ' ', text_str, flags=re.MULTILINE)
+    text_str = RE_NOALPHA.sub(' ', text_str)
+    text_str = RE_WSPACE.sub(' ', text_str)
 
     return text_str
 
