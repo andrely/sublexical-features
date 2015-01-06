49844a2b746c1575789f3940989b50c098a57d73
Andre Lynum <and-ly@online.no>
2015-01-06 09:32:37 +0100

    Base Wikipedia iterator off Gensim base class.

--- SharedCorpora/shared_corpora/wikipedia.py
+++ SharedCorpora/shared_corpora/wikipedia.py
@@ -1,26 +1,27 @@
-import bz2
+from gensim.corpora.textcorpus import TextCorpus
 
 from gensim.corpora.wikicorpus import filter_wiki, tokenize
 
 from wiki_extractor import process_data
 
 
-class WikiDump(object):
-    def __init__(self, dump_fn, limit=None, tokenizer=None, stopwords=None):
-        super(WikiDump, self).__init__()
-
-        self.dump_fn = dump_fn
-        self.limit = limit
+class WikidumpCorpus(TextCorpus):
+    def __init__(self, input=None, tokenizer=None, stopwords=None, limit=None):
         self.tokenizer = tokenizer or tokenize
         self.stopwords = stopwords or []
+        self.limit = limit
 
-    def __iter__(self):
-        with bz2.BZ2File(self.dump_fn) as f:
-            page_count = 0
-            for _, _, text in process_data(f):
-                if self.limit and page_count > self.limit:
-                    break
+        super(WikidumpCorpus, self).__init__(input)
 
-                yield [tok for tok in self.tokenizer(filter_wiki(text)) if tok not in self.stopwords]
+    def get_texts(self):
+        length = 0
 
-                page_count += 1
+        for _, _, text in process_data(self.input):
+            length += 1
+
+            yield [tok for tok in self.tokenizer(filter_wiki(text)) if tok not in self.stopwords]
+
+            if self.limit and length >= self.limit:
+                break
+
+            self.length = length
