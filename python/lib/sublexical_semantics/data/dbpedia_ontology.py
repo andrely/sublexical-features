102a2cfdb4c47d0bf25ede881a5ea5185b08cab1
André Lynum <and-ly@online.no>
2016-07-21 21:44:14 +0200

    DBpedia bag of words baseline. Stratified dataset subsampling.

---
+++ python/lib/sublexical_semantics/data/dbpedia_ontology.py
@@ -0,0 +1,148 @@
+import os
+
+import bs4
+import pandas
+import requests
+from pandas.core.frame import DataFrame
+
+SUBCLASSES_QUERY = """
+SELECT ?class
+ WHERE {
+     ?class rdfs:subClassOf <%s> .
+ }
+"""
+
+ONTOLOGY_CLASS_COUNT_QUERY = """
+select COUNT DISTINCT ?type {
+   ?type a owl:Class .
+}
+"""
+
+ONTOLOGY_CLASS_QUERY = """
+select ?type {
+   ?type a owl:Class .
+}
+"""
+
+ONTOLOGY_CLASS_RESOURCE_COUNT_QUERY = """
+PREFIX ontology:<http://dbpedia.org/ontology/>
+SELECT COUNT DISTINCT(?person)
+WHERE {
+?person rdf:type ontology:%s .
+}
+"""
+
+TITLE_AND_ABSTRACT_QUERY = """
+PREFIX ontology:<http://dbpedia.org/ontology/>
+SELECT DISTINCT(?person) ?title ?abstract
+WHERE {
+?person rdf:type ontology:%s .
+?person <http://dbpedia.org/ontology/abstract> ?abstract .
+?person foaf:name ?title .
+FILTER langMatches(lang(?abstract),'en')
+}
+ORDER BY %s(?person)
+OFFSET %s
+"""
+
+
+DISTINCT_CLASSES = ['SportsTeam', 'SocietalEvent', 'MeanOfTransportation', 'Plant', 'EducationalInstitution',
+                   'OfficeHolder', 'WrittenWork', 'NaturalPlace', 'Building', 'Company',
+                   'Infrastructure', 'Film', 'SoccerPlayer', 'Artist']
+
+
+def dbpedia_ontology_dataset(data_path):
+    dataframes = []
+
+    for category in DISTINCT_CLASSES:
+        df = DataFrame.from_csv(os.path.join(data_path, '%s.csv' % category.lower()),
+                                encoding='utf8', index_col=False)
+        df.dropna(inplace=True)
+        df['category'] = category
+        df.drop_duplicates(subset=['abstract'], inplace=True)
+
+        dataframes.append(df)
+
+    full_df = pandas.concat(dataframes)
+    full_df.reset_index(drop=True, inplace=True)
+
+    return full_df
+
+
+def download_dbpedia_ontology_dataset(dataset_path):
+    for category in DISTINCT_CLASSES:
+        df = get_resource_data_for_class(category)
+
+        df.to_csv(os.path.join(dataset_path, '%s.csv' % category.lower()),
+                  index=False, encoding='utf8')
+
+
+def get_resource_data_for_class(class_name):
+    data = []
+
+    for offset in [0, 10000, 20000, 30000]:
+        resp = requests.get('http://dbpedia.org/sparql',
+                            {'query': TITLE_AND_ABSTRACT_QUERY % (class_name, 'ASC', offset)})
+        doc = bs4.BeautifulSoup(resp.content)
+        results = [result for result in doc.findAll('result')]
+        inner_data = [(r.find('binding', {'name': 'title'}).text, r.find('binding', {'name': 'abstract'}).text)
+                      for r in results]
+
+        data += inner_data
+
+    for offset in [0, 10000, 20000, 30000]:
+        resp = requests.get('http://dbpedia.org/sparql',
+                            {'query': TITLE_AND_ABSTRACT_QUERY % (class_name, 'DESC', offset)})
+        doc = bs4.BeautifulSoup(resp.content)
+        results = [result for result in doc.findAll('result')]
+        inner_data = [(r.find('binding', {'name': 'title'}).text, r.find('binding', {'name': 'abstract'}).text)
+                      for r in results]
+
+        data += inner_data
+
+    return pandas.DataFrame(data, columns=['title', 'abstract']).drop_duplicates()
+
+
+def get_count_for_class(class_name):
+    resp = requests.get('http://dbpedia.org/sparql',
+                        {'query': ONTOLOGY_CLASS_RESOURCE_COUNT_QUERY % class_name})
+    doc = bs4.BeautifulSoup(resp.content)
+    return int(doc.find('literal').text)
+
+
+def get_classes():
+    resp = requests.get('http://dbpedia.org/sparql', {'query': ONTOLOGY_CLASS_QUERY})
+    doc = bs4.BeautifulSoup(resp.content)
+
+    return set([uri.text.split('/')[-1] for uri in doc.findAll('uri') if 'ontology' in uri.text])
+
+
+def get_classes_and_counts():
+    return [(n, get_count_for_class(n)) for n in get_classes()]
+
+
+def ontology_tree_inner(root):
+    leaves = root.findAll('li', recursive=False)
+
+    result = {}
+
+    for leaf in leaves:
+        c = leaf.find('a', href=True, recursive=False)
+
+        if c:
+            n = c['href']
+
+            count = get_count_for_class(n)
+            b = leaf.find('ul', recursive=False)
+            result[n] = ontology_tree(b)
+
+    return result
+
+
+def ontology_tree():
+    resp = requests.get('http://mappings.dbpedia.org/server/ontology/classes/')
+    doc = bs4.BeautifulSoup(resp.content)
+
+    return ontology_tree_inner(doc.find('ul'))
+
+
---
+++ python/replication/zhang-char-convnets-text-class.py
@@ -0,0 +1,86 @@
+import logging
+from argparse import ArgumentParser
+
+from numpy import round, sum, int
+from numpy.random.mtrand import choice
+from pandas import concat
+from sklearn.cross_validation import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.grid_search import GridSearchCV
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.metrics.classification import accuracy_score
+from sklearn.pipeline import Pipeline
+
+from sublexical_semantics.data.dbpedia_ontology import dbpedia_ontology_dataset
+
+
+def stratified_sample(df, size=None, target_col=None):
+    if not size or not target_col:
+        raise ValueError
+
+    target_counts = df[target_col].value_counts()
+    total = sum(target_counts.values)
+    stratified_counts = zip(target_counts.index.values, round(target_counts.values * size / total).astype(int))
+    sampled_dfs = [df[df[target_col] == target].iloc[choice(target_counts[target], c, replace=False)]
+                   for target, c in stratified_counts]
+
+    return concat(sampled_dfs)
+
+
+
+def dbpedia_bwords(sample=None):
+    df, input, target = get_dbpedia_data(size=sample)
+
+    X, X_, y, y_ = train_test_split(input, target, stratify=target, test_size=.2)
+
+    model = Pipeline([('vect', CountVectorizer(max_features=50000)), ('cls', LogisticRegression())])
+    grid = GridSearchCV(model, {'cls__C': [.0001, .0003, .001, .003, .01, .03, .1, .3, 1, 3, 10, 30, 100]},
+                        n_jobs=4, verbose=1, cv=5)
+    grid.fit(X, y)
+
+    print(accuracy_score(y_, grid.best_estimator_.predict(X_)))
+
+
+def get_dbpedia_data(size=None):
+    logging.info("Reading DBpedia dataset")
+    df = dbpedia_ontology_dataset('../../data/dbpedia-ontology/')
+
+    if size:
+        logging.info("Using sample of %d data points" % size)
+
+        df = stratified_sample(df, size=size, target_col='category')
+
+    input = [' '.join([t, a]) for t, a in zip(df.title, df.abstract)]
+    target = df.category
+
+    return df, input, target
+
+
+def dbpedia_summary():
+    df, x, y = get_dbpedia_data()
+
+    print('Documents: %d' % len(x))
+    print(df.category.value_counts())
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-m", "--method")
+    parser.add_argument("-d", "--dataset")
+    parser.add_argument("-u", "--summary", default=False, action='store_true')
+    parser.add_argument("-s", "--sample", default=None)
+    opts = parser.parse_args()
+
+    method = opts.method
+    dataset = opts.dataset
+    summary = opts.summary
+    sample = int(opts.sample) if opts.sample else None
+
+    if dataset == 'dbpedia' and summary:
+        dbpedia_summary()
+    elif dataset == 'dbpedia' and method == 'bagwords':
+        dbpedia_bwords(sample=sample)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    main()
