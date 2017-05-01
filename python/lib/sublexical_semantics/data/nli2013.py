6ee348a95d3eb76e03e360127df172c36d044684
André Lynum <and-ly@online.no>
2017-05-01 17:20:02 +0200

    Cirrus wikipedia and NLI2013 data readers.

---
+++ python/lib/sublexical_semantics/data/nli2013.py
@@ -0,0 +1,36 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+FOLD_CSV = {
+    'train': ('NLI_2013_Training_Data', 'index-training.csv'),
+    'dev': ('NLI_2013_Development_Data', 'index-dev.csv')
+}
+
+
+def nli2013_dataset(dataset_path, fold=None):
+    if not fold:
+        fold = 'train'
+
+    descr = pd.read_csv(os.path.join(dataset_path, FOLD_CSV[fold][0], FOLD_CSV[fold][1]), header=None,
+                        names=['filename', 'prompt', 'l1', 'difficulty'])
+
+    for i in range(len(descr)):
+        row = descr.iloc[i]
+
+        fn = row.filename
+
+        with open(os.path.join(dataset_path, FOLD_CSV[fold][0], 'tokenized', fn)) as f:
+            text = f.read()
+
+        yield {'text': text, 'l1': row.l1, 'prompt': row.prompt, 'difficulty': row.difficulty}
+
+
+def nli2013_df(dataset_path, fold=None, limit=None):
+    data = list(nli2013_dataset(dataset_path, fold=fold))
+
+    if limit:
+        data = np.random.choice(data, size=limit, replace=False)
+
+    return pd.DataFrame(data)
